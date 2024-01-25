279f03778bc4caec45dfe7220abc991265c8559f
Daymox <51426059+Daymox@users.noreply.github.com>
2024-01-25 15:54:10 -0500

    Update cocineros.js

--- ProyectoCocina/Cocineros/cocineros.js
+++ ProyectoCocina/Cocineros/cocineros.js
@@ -2,22 +2,4 @@ export class Cocinero {
 	constructor() {
 		this.especialidad = '';
 	}
-
-	cambiarEspecialidad(nuevaEspecialidad) {
-		this.especialidad = nuevaEspecialidad;
-	}
-
-	cocinarPlato() {
-		// Método abstracto
-	}
-
-	catarPlato(plato) {
-		// Método abstracto
-	}
-
-	participarTorneo(torneo) {
-		const plato = this.cocinarPlato();
-		const puntuacionPlato = torneo.evaluarPlato(plato, this);
-		torneo.agregarParticipante(this, plato, puntuacionPlato);
-	}
 }
